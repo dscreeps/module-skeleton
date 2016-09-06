8db1e7adbd45d04d320b8624dcccc89855118c95
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-06 14:36:16 +0200

    Extract Gruntfile.js into own repo to share it with all modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,3 @@
 'use strict';
 
-module.exports = grunt => {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        banner: "'use strict';\n\n"
-      },
-      dist: {
-        src: ['src/module.js', 'src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      },
-      live: {
-        src: ['src/module.js', 'src/**/*.js'],
-        dest: '../<%= pkg.name %>.js'
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
-};
+module.exports = require('dscreeps-grunt');
--- dist/dscreeps-module-skeleton.js
+++ dist/dscreeps-module-skeleton.js
@@ -1,3 +1,3 @@
-'use strict';
+'use strict'; // v1.0.0
 
 module.exports = () => {};
